1f937323688658c540b689480ccc11a6929dc1c2
Rishon <rishonrajesh13@gmail.com>
2025-12-31 17:16:13 -0800

    Notes on typedef added

---
+++ 14_typedef.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+  // The typedef keyword helps us shorten the type name of struct type variables when we use them 
+  // TECHNICAL DEFINITION: typedef keyword helps us create an alias (new name) for an existing type
+
+  // E.g
+  /*
+    struct Person {
+      char *name,
+      int age
+    };
+
+    can also be written as:
+
+    struct Person {
+      char *name,
+      int age
+    } person;
+
+    and then instead of writing struct Person everytime we want to initialize a variable of this struct type,
+    we can simply type person = {'Michael', 23} instead of Struct Person = {'Michael', 23}
+
+    Here, person is the alias for the struct Person type
+
+
+  */
+}
